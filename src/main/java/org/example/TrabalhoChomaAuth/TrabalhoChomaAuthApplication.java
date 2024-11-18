2769227a1e15052d8acd81fdb55100616ac7ce7c
JuninhoAldi <juninhoaldi@outlook.com>
2024-11-18 12:06:21 -0300

    V1

---
+++ src/main/java/org/example/TrabalhoChomaAuth/TrabalhoChomaAuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.TrabalhoChomaAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoChomaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoChomaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/TrabalhoChomaAuth/TrabalhoChomaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.TrabalhoChomaAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoChomaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
